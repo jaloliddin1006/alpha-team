5aff150ee6aa92553d42da8951b8bb184508b674
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2024-01-28 10:09:01 +0500

    update template

--- apps/ict/urls.py
+++ apps/ict/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('vacancies/', views.VacancyListView.as_view(), name='vacancies'),
 ]
--- apps/ict/views.py
+++ apps/ict/views.py
@@ -9,14 +9,22 @@ from django.core.paginator import Paginator
 
 class HomePageView(View):
     def get(self, request, *args, **kwargs):
+        vacancies = Vacancy.objects.all()[:10]
+        context = {
+            'vacancies': vacancies
+        }
+        return render(request, 'home.html', context)
+    
+
+
+class VacancyListView(View):
+    def get(self, request, *args, **kwargs):
         vacancies = Vacancy.objects.all()
         paginator = Paginator(vacancies, 10)
         page_number = request.GET.get('page')
         vacancies = paginator.get_page(page_number)
         context = {
             'vacancies': vacancies
-        }
-        
-    
-        return render(request, 'home.html', context)
+        }    
+        return render(request, 'news_list.html', context)
     
